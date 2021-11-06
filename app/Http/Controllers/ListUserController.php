554e88f31a6df231b4c68a2b1fa2f3e24cbf4643
abrahamluiss <abraham105@hotmail.com>
2021-11-06 17:44:30 -0500

    list user index

--- app/Http/Controllers/ListUserController.php
+++ app/Http/Controllers/ListUserController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\UserLudik;
 use Illuminate\Http\Request;
 
 class ListUserController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        $users = UserLudik::get();
+        // dd(json_encode($users));
+        return view('listusers', compact('users'));
+    }
 }
